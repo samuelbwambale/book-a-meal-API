96e104e9dc072a82be09e4a6eb649a28a2f0b75f
Samuel Bwambale <simplesam63@gmail.com>
2018-04-30 15:51:32 +0300

    updated meals_resource

--- app/api/meals_resource.py
+++ app/api/meals_resource.py
@@ -10,24 +10,24 @@ for i in range(1, 10):
 class MealsResource(Resource):
     def get(self):
         return {
-            'meals':  [{'id': meal.id, 'price': meal.price, 'description': meal.desc} for meal in meals]
+            'meals':  [{'id': meal.id, 'price': meal.price, 'desc': meal.desc} for meal in meals]
         }
 
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('price', type=float,
                             required=True, help='Price is required')
-        parser.add_argument('description', type=str,
+        parser.add_argument('desc', type=str,
                             required=True, help='Description is required')
         params = parser.parse_args()
         price = params['price']
-        desc = params['description']
+        desc = params['desc']
 
         meal = Meal(price=price, desc=desc)
         meal.addMeal()
 
         return {
-            'meals':  [{'id': meal.id, 'price': meal.price, 'description': meal.desc} for meal in meals]
+            'meals':  [{'id': meal.id, 'price': meal.price, 'desc': meal.desc} for meal in meals]
         }, 200
 
 
@@ -35,22 +35,22 @@ class MealResource(Resource):
     def put(self, meal_id):
         parser = reqparse.RequestParser()
         parser.add_argument('price', type=float, required=True)
-        parser.add_argument('description', type=str, required=True)
+        parser.add_argument('desc', type=str, required=True)
         params = parser.parse_args()
         price = params['price']
-        desc = params['description']
+        desc = params['desc']
         for meal in meals:
             if meal['id'] == meal_id:
                 meal['desc'] = desc
                 meal['price'] = price
-                return {'meals': {'id': meal.id, 'price': meal.price, 'description': meal.desc}}
+                return {'meals': {'id': meal.id, 'price': meal.price, 'desc': meal.desc}}
 
     def delete(self, meal_id):
         for meal in meals:
             if meal['id'] == meal_id:
                 meal.deleteMeal()
                 return {
-                    'meals':  [{'id': meal.id, 'price': meal.price, 'description': meal.desc} for meal in meals]
+                    'meals':  [{'id': meal.id, 'price': meal.price, 'desc': meal.desc} for meal in meals]
                 }, 200
 
 
