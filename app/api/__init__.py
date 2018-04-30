72078f657e2f23f7176d9cfcc4948f136170b2aa
Samuel Bwambale <simplesam63@gmail.com>
2018-04-30 15:50:56 +0300

    added user_resource

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -8,8 +8,12 @@ api = Api(apiv1)
 from .meals_resource import MealsResource
 from .meals_resource import MealResource
 from .menus_resource import MenuResource
-
-
 api.add_resource(MealsResource,'/meals')
 api.add_resource(MealResource,'/meals/<int:meal_id>')
 api.add_resource(MenuResource,'/menu')
+
+from .users_resource import UsersResource
+from .users_resource import UserRegister
+from .users_resource import UserLogin
+api.add_resource(MealsResource,'/users')
+api.add_resource(MealsResource,'/auth/signup')
