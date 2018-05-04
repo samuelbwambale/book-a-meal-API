6077c6ec4e2f17ca040c10646411f197e9493221
Samuel Bwambale <31615608+samuelbwambale@users.noreply.github.com>
2018-05-04 15:21:01 +0300

    Revert "Ft setup menu endpoint 156978740"

--- app/models/menu.py
+++ app/models/menu.py
@@ -1,74 +1,20 @@
-from datetime import datetime
-from meals import Meal, get_meal_by_id
+menus = []
 
-
-menus = [
-    {'id': 222,
-     'forToday': False,
-     'meals': [{
-         "price": "something soemthing",
-         "id": 8,
-         "description": 1001
-     },
-         {
-         "price": "something soemthing",
-         "id": 9,
-         "description": 1002
-     },
-         {
-         "price": "something soemthing",
-         "id": 15,
-         "description": 1003
-     }]},
-
-    {'id': 111,
-     'forToday': True,
-     'meals': [{
-             "price": "something soemthing",
-             "id": 3,
-         "description": 1001
-     },
-         {
-         "price": "something soemthing",
-         "id": 18,
-         "description": 1002
-     },
-         {
-         "price": "something soemthing",
-         "id": 20,
-         "description": 1003
-     }]}
-]
-
-def get_menu_by_id(menu_id):
-    for menu in menus:
-        if menu['id'] == menu_id: 
-            return menu
 
 class Menu:
-    count = 0
-    def __init__(self, forToday, meals):
-        Menu.count = Menu.count + 1
-        self.id = Menu.count
-        self.forToday = forToday
+    def __init__(self, date, meals):
+        self.id = random.randint(1,6)
+        self.date = date
         self.meals = []
     
-    def add_meal_to_menu(self, meal_id):
-        selected_meal = get_meal_by_id(meal_id)
-        self.meals.append(selected_meal)
+    def addMenu(self):
+        menus.append(self)
 
-    def updateMenu(self, forToday, meals):
-        self.forToday = forToday
+    def updateMenu(self, date, meals):
+        self.date = date
         self.meals = meals
 
     def deleteMenu(self):
         menus.remove(self)
         return "Success"
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'forToday': self.forToday, 
-            'meals': [meal.to_dict() for meal in self.meals]
-        }
         		
