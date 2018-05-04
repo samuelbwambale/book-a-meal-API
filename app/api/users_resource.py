3bc19aa1af7f049d6b848d8d4dedf4d97436d07f
Samuel Bwambale <31615608+samuelbwambale@users.noreply.github.com>
2018-05-04 16:12:48 +0300

    edited user resource to resolve a merge conflict

--- app/api/users_resource.py
+++ app/api/users_resource.py
@@ -29,14 +29,6 @@ class UserRegister(Resource):
         if user:
             return {'message': 'User {} already exists'.format(data['username'])}
 
-<<<<<<< HEAD
-        user = get_user_by_username(param['username'])
-        if user:
-            return {'message': 'User {} already exists'. format(param['username'])}
-        else:
-            user.addUser()
-            return {'user': user.to_dict()}, 201
-=======
         user = User(
         username = data['username'],
         password = User.generate_hash(data['password']),
@@ -54,9 +46,6 @@ class UserRegister(Resource):
                 }, 201
         except Exception as err:
             return {'message': '{}'.format(err)}, 500
->>>>>>> 694b6eda4948ee5fa436ec5fc256fad2c5e615b8
-       
-
 
 class UserLogin(Resource):
     def post(self):
@@ -67,15 +56,7 @@ class UserLogin(Resource):
                     help='Password is required')
         param = parser.parse_args()
         user = get_user_by_username(param['username'])
-<<<<<<< HEAD
-        #user = [u for u in users if param['username'] == u['username']]
 
-        if not user:
-            return {'message': 'User {} does not exist'. format(param['username'])}
-      
-        if user['password'] == param['password']:
-            return {'message': 'Logged in as {}'. format(user['username'])}
-=======
         #import pdb; pdb.set_trace()
         if not user:
             return {'message': 'User {} does not exist'. format(param['username'])}
@@ -87,50 +68,6 @@ class UserLogin(Resource):
                 'access_token': access_token,
                 'refresh_token': refresh_token
                 }
->>>>>>> 694b6eda4948ee5fa436ec5fc256fad2c5e615b8
         else:
             return {'message': 'Wrong credentials'}
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class MealResource(Resource):
-    def put(self, meal_id):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price', type=float, required=True)
-        parser.add_argument('desc', type=str, required=True)
-        params = parser.parse_args()
-        price = params['price']
-        desc = params['desc']
-        for meal in meals:
-            if meal['id'] == meal_id:
-                meal['desc'] = desc
-                meal['price'] = price
-                return {'meals': {'id': meal.id, 'price': meal.price, 'desc': meal.desc}}
-
-    def delete(self, meal_id):
-        for meal in meals:
-            if meal['id'] == meal_id:
-                meal.deleteMeal()
-                return {
-                    'meals':  [{'id': meal.id, 'price': meal.price, 'desc': meal.desc} for meal in meals]
-                }, 200
-
-
